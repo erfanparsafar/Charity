4414024131a8bb3d61cba6c35b2cad8b643ad701
erfanparsa <erfanparsafar84@gmail.com>
2024-09-04 20:28:31 +0330

    Make UserRegistration view with serializer and generics

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,6 +1,33 @@
 from rest_framework import serializers
-
+from django.contrib.auth import get_user_model
 from .models import User
 
+User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    password = serializers.CharField(write_only=True)
+   
+    class Meta:
+       model = User
+       fields =  ['username', 'password', 'phone', 'address', 'gender', 'age', 'description' ,'first_name', 'last_name' ,'email']
+         
+    def create(self,validated_data ):
+       user = User.objects.create_user(
+           username=validated_data['username'],
+           password=validated_data['password'],
+           phone=validated_data.get('phone', ''),
+           address=validated_data.get('address', ''),
+           gender=validated_data.get('gender', User.Gender.UNSET),
+           age=validated_data.get('age', None),
+           description=validated_data.get('description', ''),
+           first_name = validated_data.get('first_name',),
+           last_name = validated_data.get('last_name',),
+           email = validated_data.get('email', ''),
+       )  
+       return user
+       
+       
+       
+       
+       
+       
+       
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,13 +3,15 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 
+User = get_user_model()
 
 class LogoutAPIView(APIView):
     pass
 
 
 class UserRegistration(generics.CreateAPIView):
-    pass
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
